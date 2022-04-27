c2d0e9c5b54c6821f826d79edba8863654fcb760
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-04-28 02:23:48 +0900

    Release to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   resources :users, only: [:show]
   resources :posts
   root 'posts#index'
