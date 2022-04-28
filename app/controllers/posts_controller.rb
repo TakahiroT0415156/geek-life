41f19d6c6299ecd95535f0392e8f0a12fd5fb041
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-04-28 13:23:20 +0900

    リリース

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     if params[:search].present? && params[:tag_id].present?
